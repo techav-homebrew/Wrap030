a242f233c134b1430e6e958c46b61156396125b5
techav <76832805+techav-homebrew@users.noreply.github.com>
2025-04-12 23:04:15 -0500

    DRAM Control
    - rework DRAM control logic to be cleaner

--- ExpansionBoards/Memory/Logic/Wrap030 Memory Glue.sv
+++ ExpansionBoards/Memory/Logic/Wrap030 Memory Glue.sv
@@ -25,7 +25,7 @@ module wrap030_dram_glue (
     output  wire [3:0]      memCas_n,   // dram col strobe          55/52/54/51
     output  wire [1:0]      memRas_n,   // dram row strobe              49/50
     output  wire            memWe_n,    // dram write enable            57
-    output  wire            memBufE_n,  // dram buffer enable           5
+    output  wire            memBufE_n   // dram buffer enable           5
 
 //  output  wire            timeIrq_nz, // timer interrupt  (io5)       11
 // spare I/O
@@ -35,19 +35,24 @@ module wrap030_dram_glue (
 //    inout   wire            io8,        //                              9
 //    inout   wire            io11,       //                              8
 //    inout   wire            io13,       //                              6
-    output  wire [3:0]      stateDebug
 );
 
+// internal registers
+reg [3:0] timingState;          // state machine current state
+reg [3:0] initCount;            // startup initialization sequence
+reg [6:0] refreshTimer;         // counter until time to run refresh cycle
+reg refreshCall;                // set when time to run refresh cycle
+reg refreshAck;                 // set when starting refresh cycle
+reg rOverlay;                   // startup ROM overlay
 
-//assign stateDebug = timingState;
-/*always @(negedge busClk, negedge busReset_n) begin
-    if(!busReset_n) stateDebug <= sIDL;
-    else stateDebug <= nextState;
-end*/
+// internal wires
+wire [3:0] nextState;           // state machine next state
+wire UUDn, UMDn, LMDn, LLDn;    // data bus byte select signals
+wire memCE_n;                   // bus is trying to address RAM
 
-
-// data bus byte & word select signals
-wire UUDn, UMDn, LMDn, LLDn;
+/******************************************************************************
+ * bus decoding
+ *****************************************************************************/
 always_comb begin
     UUDn = ~(
         busRW_n | (~busAddr[0] & ~busAddr[1])
@@ -66,390 +71,226 @@ always_comb begin
         busRW_n | (busAddr[0] & busSiz[0] & busSiz[1]) | (~busSiz[0] & ~busSiz[1]) | 
         (busAddr[0] & busAddr[1]) | (busAddr[1] & busSiz[1])
     );
-    //UDn = ~(~busAddr[0] | busRW_n);
-    //LDn = ~(~busSiz[0] | busSiz[1] | busAddr[0] | busRW_n);
-end
-/*
-assign byteSELn[0] = LLDn;
-assign byteSELn[1] = LMDn;
-assign byteSELn[2] = UMDn;
-assign byteSELn[3] = UUDn;
-*/
-
-
-
-// initialization timer
-reg [3:0] initCount;
-always @(posedge busClk, negedge busReset_n) begin
-    if(!busReset_n) begin
-        //initCount <= 13'h1388;          // 5000 cycles @ 25MHz = 200us
-        initCount <= 4'b0111;             // 7 cycles @ 25MHz = 280ns
-    end else begin
-        if(initCount > 0) initCount <= initCount - 13'd1;
-        else initCount <= 4'd0;
-    end
 end
 
-
-// refresh timing counter
-reg [6:0] refreshTimer;
-reg refreshCall, refreshAck;
-reg [3:0] initRfshCount;
-always @(posedge busClk, negedge busReset_n) begin
-    if(!busReset_n) begin
-        refreshTimer <= 0;
-        refreshCall <= 0;
-//    end else if(refreshTimer >= 9'h186) begin   // 390 cycles @ 25MHz = 15.6us
-    end else if(refreshTimer >= 7'h7E) begin    // 126 cycles @ 25MHz = 5.04 us
-        refreshCall <= 1;
-        refreshTimer <= 0;
-    end else if(initRfshCount >= 4'h8) begin
-        refreshTimer <= refreshTimer + 7'h001;
-        if(refreshAck) refreshCall <= 0;
-        else refreshCall <= refreshCall;
-    end
+always_comb begin
+    if(!busAS_n && !busAddr31 && (busFC[0] ^ busFC[1])) begin
+        // this looks like a RAM access cycle
+        if(!busRW_n) memCE_n = 0;
+        else if(rOverlay && busAddr >= 24'h080000) memCE_n = 0;
+        else if(!rOverlay) memCE_n = 0;
+        else memCE_n = 1;
+    end else memCE_n = 1;
 end
 
-always @(negedge busClk, negedge busReset_n) begin
-    if(!busReset_n) begin
-        refreshAck <= 0;
-    end else begin
-        if(nextState == sRC0) begin
-            refreshAck <= 1;
-        end else begin
-            refreshAck <= 0;
-        end
-    end
-end
-
-// initRfshCount counts 8 refresh cycles immediately following the reset
-// initialization hold sequence. It is incremented when the state machine
-// is in the middle of the refresh sequence, and held there until reset
-always @(negedge busClk, negedge busReset_n) begin
-    if(!busReset_n) begin
-        initRfshCount <= 0;
-    end else begin
-        if(initRfshCount < 4'h8) begin
-            if(timingState == sRR2) begin
-                initRfshCount <= initRfshCount + 4'd1;
-            end
-        end
-    end
-end
-
-
-
-always @(negedge busClk, negedge busReset_n) begin
-    if(!busReset_n) begin
-        stateDebug <= 4'h0;
-    end else begin
-        case(nextState)
-            sIDL: stateDebug <= 4'h0;
-            sCR0: stateDebug <= 4'h1;
-            sCC0: stateDebug <= 4'h2;
-            sCC1: stateDebug <= 4'h3;
-            sBST: stateDebug <= 4'h4;
-            sBND: stateDebug <= 4'h5;
-            sRC0: stateDebug <= 4'h6;
-            sRR0: stateDebug <= 4'h7;
-            sRR1: stateDebug <= 4'h8;
-            sRR2: stateDebug <= 4'h9;
-            sINIT: stateDebug <= 4'hA;
-            sREG: stateDebug <= 4'hB;
-            default: stateDebug <= 4'hF;
-        endcase
-    end
-end
-
-// primary DRAM controller state machine
+/******************************************************************************
+ * primary DRAM controller state machine
+ *****************************************************************************/
 parameter
     sIDL    =   0,  //  Idle state
     sCR0    =   1,  //  Cycle RAS state 0
     sCC0    =   2,  //  Cycle CAS state 0
     sCC1    =   3,  //  Cycle CAS state 1
-    sBST    =   4,  //  Burst Cycle state
-    sBND    =   5,  //  Burst End state
-    sRC0    =   6,  //  Refresh CAS state 0
-    sRR0    =   7,  //  Refresh RAS state 0
-    sRR1    =   8,  //  Refresh RAS state 1
-    sRR2    =   9,  //  Refresh RAS state 2
-    sINIT   =  10,  //  Startup initialization state
-    sREG    =  11;  //  Write configuration registers state
-reg [3:0] timingState, nextState;
+    sEND    =   4,  //  Cycle end state
+    sRC0    =   5,  //  Refresh CAS state 0
+    sRR0    =   6,  //  Refresh RAS state 0
+    sRR1    =   7,  //  Refresh RAS state 1
+    sRR2    =   8,  //  Refresh RAS state 2
+    sINIT   =   9;  //  Startup initialization state
 
-always @(negedge busClk, negedge busReset_n) begin
-    if(!busReset_n) begin
-        timingState <= sINIT;
-    end else begin
-        timingState <= nextState;
-    end
-end
-
-//always @(timingState, ramCEn, cpuCBREQn, refreshCall, initRfshCount, initCount, cpuAddr, cpuRWn) begin
-always @(*) begin
-    nextState = timingState;
+always_comb begin
     case(timingState)
+        // handle startup initialization
         sINIT: begin
-            // Startup initialization state
-            // hold here until initCount is 0
-            // then move on to sRC0 to start a series of 8 refresh cycles
-            if(initCount > 0) begin
-                nextState = sINIT;
-            end else begin
-                nextState = sRC0;
-            end
-        end
-        sIDL: begin
-            // Idle state
-            // if time for refresh, then move to sRC0
-            // else if time for CPU cycle then move to sCR0
-            // else state at Idle
-            if(refreshCall) begin
-                nextState = sRC0;
-            end else if(!busAS_n && !busAddr31 && (busFC[0] ^ busFC[1])) begin
-                // this looks like a RAM access cycle ...
-                if(!busRW_n) begin
-                    // always handle writes to RAM
-                    nextState = sCR0;
-                end else if(rOverlay && busAddr >= 24'h00080000) begin
-                    // always read RAM above $0080,0000
-                    nextState = sCR0;
-                end else if(!rOverlay) begin
-                    // overlay is disabled, read RAM
-                    nextState = sCR0;
-                end else begin
-                    // we'll let ROM handle this one
-                    nextState = sIDL;
-                end
-            end else begin
-                // we're not currently being addressed
-                nextState = sIDL;
-            end
+            nextState = sRC0;
         end
 
-        // normal access cycle sequence
+        // idle state is the launching point for other cycles
+        sIDL: begin
+            if(refreshCall) nextState = sRC0;
+            else if(!memCE_n) nextState = sCR0;
+            else nextState = sIDL;
+        end
+
+        // main memory cycle sequence
         sCR0: nextState = sCC0;
         sCC0: nextState = sCC1;
-        sCC1: begin
-            // Cycle CAS state 1
-            // if cache burst, then proceed to sBST
-            // else progress to sIDL to end cycle
-            
-            /*
-            if(!busCbReq_n) begin
-                nextState = sBST;
-            end else begin
-                nextState = sIDL;
-            end
-            */
-
-            // we don't have burst request here, so just go to idle
-            //nextState = sIDL;
-            
-            // hold in this state until CPU deasserts AS
-            if(busAS_n) nextState = sIDL;
-            else nextState = sCC1;
-        end
-        sBST: begin
-            // Cycle burst state
-            // if cpu cache burst request still asserted then proceed to sCC1
-            // else proceed to sBND to end cycle
-
-            /*
-            if(!cpuCbReq_n) begin
-                nextState = sCC1;
-            end else begin
-                nextState = sBND;
-            end
-            */
-
-            // we don't have burst request here, so just go to idle
-            nextState = sIDL;
+        sCC1: nextState = sEND;
+        sEND: begin
+            if(refreshCall) nextState = sRC0;
+            else if(!busAS_n) nextState = sEND;
+            else nextState = sIDL;
         end
 
-        // Cylce burst end; always proceed to sIDL to end cycle
-        sBND: nextState = sIDL;
-
-        // CBR Refresh sequence
+        // refresh cycle sequence
         sRC0: nextState = sRR0;
         sRR0: nextState = sRR1;
         sRR1: nextState = sRR2;
         sRR2: begin
-            // Refresh cycle RAS state 2
-            // if initialization is not complete, then start another refresh cycle
-            // else proceed to sIDL
-            if(initRfshCount < 4'h8) begin
-                nextState = sINIT;
-            end else begin
-                nextState = sIDL;
-            end
+            if(initCount > 0) nextState = sRC0;
+            else nextState = sIDL;
         end
-
-        // Register write state (not implemented here either)
-        sREG: nextState = sIDL;
 
         default: nextState = sIDL;
     endcase
 end
 
+always @(posedge busClk, negedge busReset_n) begin
+    if(!busReset_n) timingState <= sINIT;
+    else timingState <= nextState;
+end
 
-// memory address output
+/******************************************************************************
+ * startup initialization
+ *****************************************************************************/
+
+// run 8 refresh cycles at startup before operation
+always @(negedge busClk, negedge busReset_n) begin
+    if(!busReset_n) initCount <= 4'h8;
+    else if(initCount > 0 && timingState == sRR2) initCount <= initCount - 4'h1;
+    else initCount <= initCount;
+end
+
+/******************************************************************************
+ * DRAM refresh
+ *****************************************************************************/
+
+// refresh cycle acknowledge
+always @(posedge busClk, negedge busReset_n) begin
+    if(!busReset_n) refreshAck <= 0;
+    else begin
+        if(nextState == sRC0) refreshAck <= 1;
+        else refreshAck <= 0;
+    end
+end
+
+// refresh timing counter
 always @(negedge busClk, negedge busReset_n) begin
     if(!busReset_n) begin
-        memAddr <= 0;
+        refreshTimer <= 0;
+        refreshCall <= 0;
+    end
+    else if(refreshTimer >= 7'h7E) begin
+        refreshTimer <= 0;
+        refreshCall <= 1;
     end else begin
-        case(nextState)
-            sCR0: begin
-                // output row address
-                memAddr[11] <= 0;
-                memAddr[10:0] <= busAddr[23:13];
-            end
-            sCC0, sBST: begin
-                // output column address
+        refreshTimer <= refreshTimer + 7'h1;
+        if(refreshAck) refreshCall <= 0;
+        else refreshCall <= refreshCall;
+    end
+end
+
+/******************************************************************************
+ * DRAM control signals
+ *****************************************************************************/
+
+// Memory Address
+always @(negedge busClk, negedge busReset_n) begin
+    if(!busReset_n) memAddr <= 0;
+    else begin
+        case(timingState)
+            sCR0, sCC0, sCC1, sEND: begin
+                // latch column address
                 memAddr[11] <= 0;
                 memAddr[10:0] <= busAddr[12:2];
             end
-        endcase
-    end
-end
-
-
-// row & column strobe outputs
-// these are double-buffered to ensure timing, given everything that goes into
-// calculating each signal (expecially CAS)
-reg[3:0] nextMemCASn;
-reg nextMemRASn;
-
-// calculate nextMemRASn
-always @(posedge busClk, negedge busReset_n) begin
-    if(!busReset_n) begin
-        nextMemRASn <= 1;
-    end else begin
-        case(nextState)
-            sCR0, sCC0, sCC1, sBST, sBND: begin
-                // cpu access cycle
-                nextMemRASn <= 0;
-            end
-            sRR0, sRR1, sRR2: begin
-                // refresh cycle
-                nextMemRASn <= 0;
-            end
             default: begin
-                nextMemRASn <= 1;
+                // continually latch row address
+                memAddr[11] <= 0;
+                memAddr[10:0] <= busAddr[23:13];
             end
         endcase
     end
 end
 
-// calculate nextMemCASn
+// Write Enable
 always @(posedge busClk, negedge busReset_n) begin
-    if(!busReset_n) begin
-        nextMemCASn <= 4'b1111;
-    end else begin
+    if(!busReset_n) memWe_n <= 1;
+    else begin
         case(nextState)
-            sCC0, sCC1, sBND: begin
-                // cpu access cycle
-                nextMemCASn[0] <= LLDn;
-                nextMemCASn[1] <= LMDn;
-                nextMemCASn[2] <= UMDn;
-                nextMemCASn[3] <= UUDn;
-            end
-            sRC0, sRR0, sRR1: begin
-                // refresh cycle
-                nextMemCASn <= 4'b0000;
-            end 
-            sINIT: begin
-                // this is to catch a special case at the end of the initialization cycle
-                if(initCount == 1) nextMemCASn <= 4'b0000;
-                else nextMemCASn <= 4'b1111;
-            end
-            default: begin
-                nextMemCASn <= 4'b1111;
-            end
+            sCR0, sCC0, sCC1, sEND: memWe_n <= busRW_n;
+            default: memWe_n <= 1;
         endcase
     end
 end
 
-// not actually output RAS/CAS signals
-always @(negedge busClk, negedge busReset_n) begin
-    if(!busReset_n) begin
-        memRas_n <= 2'b11;
-        memCas_n <= 4'b1111;
-    end else begin
-        memRas_n[1] <= 1;
-        memRas_n[0] <= nextMemRASn;
-        memCas_n <= nextMemCASn;
+// Row Address Strobe
+always @(posedge busClk, negedge busReset_n) begin
+    if(!busReset_n) memRas_n <= 2'b11;
+    else begin
+        case(nextState)
+            sCR0, sCC0, sCC1: memRas_n <= 2'b10;
+            sRR0, sRR1, sRR2: memRas_n <= 2'b00;
+            default: memRas_n <= 2'b11;
+        endcase
     end
 end
 
-
-
-// misc other control signals output
-always_comb begin
-    case(timingState)
-        sCR0, sCC0: begin
-            busDsack_nz <= 2'bZZ;
-            memWe_n <= busRW_n;
-        end
-        sCC1: begin
-            busDsack_nz <= 2'b00;
-            memWe_n <= busRW_n;
-        end
-        sBST: begin
-            busDsack_nz <= 2'bZZ;
-            memWe_n <= busRW_n;
-        end
-        sBND: begin
-            busDsack_nz <= 2'b00;
-            memWe_n <= busRW_n;
-        end
-        sREG: begin
-            busDsack_nz <= 2'b10;
-            memWe_n <= busRW_n;
-        end
-        default: begin
-            busDsack_nz <= 2'bZZ;
-            memWe_n <= busRW_n;
-        end
-    endcase
+// Column Address Strobe
+always @(posedge busClk, posedge busAS_n) begin
+    if(busAS_n) memCas_n <= 4'b1111;
+    else begin
+        case(nextState)
+            sCC0, sCC1: begin
+                memCas_n[0] <= LLDn;
+                memCas_n[1] <= LMDn;
+                memCas_n[2] <= UMDn;
+                memCas_n[3] <= UUDn;
+            end
+            sRC0, sRR0, sRR1, sRR2: memCas_n <= 4'b0000;
+            default: memCas_n <= 4'b1111;
+        endcase
+    end
 end
 
+/******************************************************************************
+ * Bus control signals
+ *****************************************************************************/
 
-// ROM overlay enable/disable
-reg rOverlay;
+// data bus buffers
+always @(posedge busClk, negedge busReset_n) begin
+    if(!busReset_n) memBufE_n <= 1;
+    else begin
+        case(nextState)
+            sCR0, sCC0, sCC1: memBufE_n <= 0;
+            sEND: begin
+                // hold buffer open until end of write cycles
+                if(!busRW_n) memBufE_n <= 0;
+                else memBufE_n <= 1;
+            end
+            default memBufE_n <= 1;
+        endcase
+    end
+end
+
+// Data Strobe Acknowledge
+always @(negedge busClk, posedge busAS_n) begin
+    if(busAS_n) busDsack_nz <= 2'bZZ;
+    else begin
+        case(timingState)
+            sCC0: busDsack_nz <= 2'b00;
+            sEND: busDsack_nz <= 2'b11;
+            default: busDsack_nz <= 2'bZZ;
+        endcase
+    end
+end
+
+/******************************************************************************
+ * Startup ROM overlay
+ *****************************************************************************/
+
 always @(negedge busClk, negedge busReset_n) begin
-    if(!busReset_n) begin
-        rOverlay <= 1;
-    end else begin
-        if(!busAS_n && busAddr31 && (busFC[0] ^ busFC[1]) && busAddr[21:19] == 3) begin
+    if(!busReset_n) rOverlay <= 1;
+    else begin
+        if(     !busAS_n 
+                && busAddr31 
+                && (busFC[0] ^ busFC[1]) 
+                && busAddr[21:19] == 3
+        ) begin
             rOverlay <= 0;
         end else begin
             rOverlay <= rOverlay;
         end
-    end
-end
-
-
-// Data bus buffers enable
-/*
-    sCR0    =   1,  //  Cycle RAS state 0
-    sCC0    =   2,  //  Cycle CAS state 0
-    sCC1    =   3,  //  Cycle CAS state 1
-    sBST    =   4,  //  Burst Cycle state
-    sBND    =   5,  //  Burst End state
-*/
-always @(negedge busClk, negedge busReset_n) begin
-    if(!busReset_n) begin
-        memBufE_n <= 1;
-    end else begin
-        case(nextState)
-            sCR0, sCC0, sCC1, sBST, sBND: begin
-                memBufE_n <= 0;
-            end
-            default: begin
-                memBufE_n <= 1;
-            end
-        endcase
     end
 end
 
